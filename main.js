4de1f76b4942b51394f75b0ea3832210c1d0c9d5
overflow326 <s.daizou@outlook.com>
2019-03-03 17:35:15 +0900

    review_1

--- main.js
+++ main.js
@@ -3,9 +3,6 @@
  *   - getElementByIdを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-const elementById = document.getElementById(`title`);
-console.log(elementById);
-
 
 /**
  * 課題2: id値が 'item-container' の要素を取得する
@@ -13,20 +10,10 @@ console.log(elementById);
  *   - console.logで取得したDOMの中身を確認する
  */
 
-const itemcontainerElementByquerySelecter = document.querySelector(`#item-container`);
-console.log(itemcontainerElementByquerySelecter);
-
-
-
 /**
  * 課題3: li要素を全て取得する
  *   - console.logで取得したDOMの中身を確認する
  */
-
-const liElementsByTagName = document.getElementsByTagName(`li`);
-console.log(liElementsByTagName);
-
-
 
 /**
  * 課題4: class値が 'item' の要素を全て取得する
@@ -34,14 +21,8 @@ console.log(liElementsByTagName);
  *   - console.logで取得したDOMの中身を確認する
  */
 
-const itemElementsByClassName = document.getElementsByClassName(`item`);
-console.log(itemElementsByClassName);
-
 /**
  * 課題5: class値が 'item' の要素を全て取得する
  *   - querySelectorAllを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
-const itemElementsByQuerySelecterAll = document.querySelectorAll(`.item`);
-console.log(itemElementsByQuerySelecterAll);
