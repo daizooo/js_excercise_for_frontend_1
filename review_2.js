b41a96c3ff614ed5eb2dcb5fb4a77d5c2570838b
overflow326 <s.daizou@outlook.com>
2019-03-04 20:27:37 +0900

    review_2

---
+++ review_2.js
@@ -0,0 +1,14 @@
+const titleElement = document.getElementById(`title`);
+console.log(titleElement);
+
+const itemContainerElement = document.querySelector(`#item-container`);
+console.log(itemContainerElement);
+
+const liElement = document.getElementsByTagName(`li`);
+console.log(liElement);
+
+const itemElement = document.getElementsByClassName(`item`);
+console.log(itemElement);
+
+const itemAllElement = document.querySelectorAll(`.item`);
+console.log(itemAllElement);
